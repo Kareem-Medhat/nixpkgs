539206662205137c208aedde77a311c0fbdfb70f
Kareem-Medhat <kareemmny@gmail.com>
2024-08-17 10:30:58 +0300

    added igpu device section

--- nixos/modules/hardware/video/nvidia.nix
+++ nixos/modules/hardware/video/nvidia.nix
@@ -126,6 +126,17 @@ in
         '';
       };
 
+      prime.igpuDeviceSection = lib.mkOption {
+        type = lib.types.str;
+        default = "";
+        example = ''
+          Option "PageFlip" "off"
+        '';
+        description = ''
+          Contents of the iGPU Device section of the X server configuration file.
+        '';
+      };
+
       prime.amdgpuBusId = lib.mkOption {
         type = busIDType;
         default = "";
@@ -405,6 +416,9 @@ in
                 ''
                 + lib.optionalString (syncCfg.enable && igpuDriver != "amdgpu") ''
                   Option "AccelMethod" "none"
+                ''
+                + ''
+                	${pCfg.igpuDeviceSection}
                 '';
             }
             ++ lib.singleton {
